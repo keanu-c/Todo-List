1a29b5fda80b664546fb02e0a750f624e7dd54ed
Keanu Chau <keanuchau@gmail.com>
2024-09-10 08:01:20 -0700

    Finish input container React

--- React-Todo-List/src/App.jsx
+++ React-Todo-List/src/App.jsx
@@ -2,23 +2,85 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputVal, setInputVal] = useState("");
+
+function writeTodo(e) {
+  setInputVal(e.target.value);
+  console.log(inputVal);
+}
+
+function addTodo() {
+  if (inputVal != "") {
+    setTodos((prevTodos) => [...prevTodos, inputVal]);
+    setInputVal("");
+  }
+}
+
+function deleteTodo(index) {
+  setTodos(prevTodos => prevTodos.filter((prevTodo, prevTodoIndex) => {
+    return prevTodoIndex != index;
+  }))
+}
+
   return (
     <main>
       <h1>To-Do List</h1>
-      <div className="input-container">
-          <input type="text" placeholder="Enter todo" id="input-todo"></input>
-          <button id="add-todo">+</button>
-      </div>
+      <InputContainer
+        inputVal={inputVal} 
+        writeTodo={writeTodo} 
+        addTodo={addTodo}>
+      </InputContainer>
       <div className="container">
-        <div className="todo">
-          <p>write python</p>
-          <div className="actions">
-            <input type="checkbox" />
-            <button>Delete</button>
+      {
+        todos.map((todo, index) => {
+          return (
+            <div className="todo" key={index}>
+            <p>{todo}</p>
+            <div className="actions">
+              <input type="checkbox" />
+              <button onClick={() => deleteTodo(index)}>Delete</button>
+            </div>
           </div>
-        </div>
+          );
+        })
+      }
       </div>
     </main>
+  );
+}
+
+function InputContainer({inputVal, addTodo, writeTodo}) {
+  return (
+      <div className="input-container">
+          <input 
+          type="text" 
+          placeholder="Enter todo" 
+          value = {inputVal} 
+          onChange={writeTodo}>
+          </input>
+          <button onClick={addTodo}>+</button>
+      </div>
+  );
+}
+
+function todoContainer({}) {
+  return (
+    <div className="container">
+    {
+      todos.map((todo, index) => {
+        return (
+          <div className="todo" key={index}>
+          <p>{todo}</p>
+          <div className="actions">
+            <input type="checkbox" />
+            <button onClick={() => deleteTodo(index)}>Delete</button>
+          </div>
+        </div>
+        );
+      })
+    }
+    </div>
   );
 }
 export default App
