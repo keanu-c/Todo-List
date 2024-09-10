bfd67ad804438db439e5fdd34c0282d8627e2929
Keanu Chau <keanuchau@gmail.com>
2024-09-10 01:27:04 -0700

    Add React html portion

--- React-Todo-List/src/App.jsx
+++ React-Todo-List/src/App.jsx
@@ -4,7 +4,20 @@ import './App.css'
 function App() {
   return (
     <main>
-      <h1>To-do List</h1>
+      <h1>To-Do List</h1>
+      <div className="input-container">
+          <input type="text" placeholder="Enter todo" id="input-todo"></input>
+          <button id="add-todo">+</button>
+      </div>
+      <div className="container">
+        <div className="todo">
+          <p>write python</p>
+          <div className="actions">
+            <input type="checkbox" />
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
