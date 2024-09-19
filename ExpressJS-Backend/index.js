cec058302472776224074fc3a5f71f3bc652b88f
Keanu Chau <keanuchau@gmail.com>
2024-09-19 15:39:44 -0700

    Create skeleton api requests

--- ExpressJS-Backend/index.js
+++ ExpressJS-Backend/index.js
@@ -18,16 +18,29 @@ const todos = [
 app.get("/", (req, res, next) => {
     res.send("Todo List Homepage");
 });
-// Returns json from todos
+// Returns JSON from todos
 app.get("/todos", (req, res) => {
     res.json(todos);
 });
-
+// Returns JSON from one Todo with specific id
 app.get("/todos/:id", (req, res) => {
     console.log(req.params.id);
     let todo = todos.filter((todo) => todo.id == req.params.id);
     res.json(todo);
 });
+
+app.post("/todos", (req, res) => {
+    todos.push()
+    res.json([]);
+})
+
+app.put("/todos/:id", (req, res) => {
+    res.json([]);
+})
+
+app.delete("/todos/:id", (req, res) => {
+    res.json([]);
+})
 
 app.listen(port, () => {
     console.log("app is listening in PORT", port);
