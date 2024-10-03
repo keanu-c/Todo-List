6293db18bb000996e7169e575dbb8a62e0b0e514
Keanu Chau <keanuchau@gmail.com>
2024-10-02 20:43:13 -0700

    Add GET todos, todo and PUT todo

--- Todo-List-PostgreSQL/server/index.js
+++ Todo-List-PostgreSQL/server/index.js
@@ -15,7 +15,12 @@ app.get('/', (req, res) => {
     res.json({msg: "Home Page"});
 });
 
-// Queries database for all todos
+// Starts server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}...`);
+});
+
+// GET all todos
 app.get("/todos", async (req, res) => {
     try {
         const todos = await pool.query("SELECT * FROM todo_table");
@@ -24,7 +29,48 @@ app.get("/todos", async (req, res) => {
         res.json(error);
     }
 });
-// Adds todos
+// GET todo with specific id
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo_table WHERE todo_id = $1", [id]);
+        res.json(todo.rows);
+    } catch (error) {
+        res.json(error);
+    }
+});
+// UPDATE todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {desc, completed} = req.body;
+        const todo = await pool.query("UPDATE todo_table SET todo_desc = $1, todo_completed = $2 WHERE todo_id = $3", [desc, completed, id]);
+        res.json({msg: "Todo updated", success: true});
+    } catch (error) {
+        res.json(error);
+    }
+});
+// DELETE todo with specific id
+/*
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const todo = await pool.query("SELECT * FROM todo_table");
+        res.json(todos.rows);
+    } catch (error) {
+        res.json(error);
+    }
+});
+// DELETE all todos
+app.delete("/todos", async (req, res) => {
+    try {
+        const todo = await pool.query("SELECT * FROM todo_table");
+        res.json(todos.rows);
+    } catch (error) {
+        res.json(error);
+    }
+});
+*/
+// Add todos
 app.post("/todos", async (req, res) => {
     try {
         const {desc, completed} = req.body;
@@ -34,8 +80,4 @@ app.post("/todos", async (req, res) => {
     } catch (error) {
         res.json(error)
     }
-})
-// Starts server
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}...`);
 });
