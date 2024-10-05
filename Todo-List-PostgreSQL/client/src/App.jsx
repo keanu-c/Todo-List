85bab6f9599f8ea8cd6972f7ed3921a372fcc7b3
Keanu Chau <keanuchau@gmail.com>
2024-10-05 12:21:48 -0700

    Add todos.map()

--- Todo-List-PostgreSQL/client/src/App.jsx
+++ Todo-List-PostgreSQL/client/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
-import axios from "axios";
+import axios from "axios"; // Library for HTTP requests
+
+axios.defaults.baseURL = "http://localhost:3000"
 
 function App() {
   const [inputTodo, setInputTodo] = useState("");
@@ -10,9 +12,11 @@ function App() {
   const [editTodoChecked, setEditTodoChecked] = useState(false);
 
   useEffect(() => {
-      const resp = axios.get("")
-  });
-  
+      const resp = axios.get("/todos").then(resp => {
+        setTodos(resp.data);
+      })
+  }, []);
+
   return (
     <div className="flex flex-col items-center gap-8 pt-8 pb-32 bg-blue-50">
       <div className="text-2xl">Todo List</div>
@@ -30,30 +34,22 @@ function App() {
       </div>
 
       <div className="flex flex-col gap-4 bg-blue-200 p-3 w-5/6 rounded-xl">
-        <div className="flex justify-between items-center bg-blue-400 text-white p-3 rounded-lg">
+        {todos.map((todo, index) => {
+          return (
+            <div className="flex justify-between items-center bg-blue-400 text-white p-3 rounded-lg">
 
-          <div className="flex gap-2 pl-1">
-            <input type="checkbox" />
-            <div className="">Read Code</div>
-          </div>
-
-          <div className="flex gap-2">
-            <button className='text-lg bg-green-600 hover:bg-green-500 text-white rounded-xl py-1 px-2'>Edit</button>
-            <button className='text-lg bg-red-600 hover:bg-red-500 text-white rounded-xl py-1 px-2'>Delete</button>
-          </div>
-        </div>
-        <div className="flex justify-between items-center bg-blue-400 text-white p-3 rounded-lg">
-
-          <div className="flex gap-2 pl-1">
-            <input type="checkbox" />
-            <div className="">Read Code</div>
-          </div>
-
-          <div className="flex gap-2">
-            <button className='text-lg bg-green-600 hover:bg-green-500 text-white rounded-xl py-1 px-2'>Edit</button>
-            <button className='text-lg bg-red-600 hover:bg-red-500 text-white rounded-xl py-1 px-2'>Delete</button>
-          </div>
-        </div>
+              <div className="flex gap-2 pl-1">
+                <input type="checkbox" />
+                <div className="">{todo.todo_desc}</div>
+              </div>
+    
+              <div className="flex gap-2">
+                <button className='text-lg bg-green-600 hover:bg-green-500 text-white rounded-xl py-1 px-2'>Edit</button>
+                <button className='text-lg bg-red-600 hover:bg-red-500 text-white rounded-xl py-1 px-2'>Delete</button>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
